24042186a6fa7d4ad86040e7fb1b7ab25e5b6ce5
vadimicus <vadim.makovsky@gmail.com>
2018-11-24 00:37:16 +0300

    added docker for mongo and modified main file

--- main.go
+++ main.go
@@ -57,16 +57,12 @@ var (
 )
 
 const (
-	waitTime = 87 // in seconds
+	waitTime = 87 // in seconds to respect twitter API limits 1000 follow calls per 24 hours
 	frame = 200
 )
 
 
 func main() {
-
-	var twClient *twitter.Client
-	var creds Creds
-	targetFollowers := make([]ToFollowUser,0)
 
 	//Bot initialization
 	bot, error := Init(&globalOpt)
@@ -74,9 +70,7 @@ func main() {
 		fmt.Println("Init Error:", error)
 	}
 
-	fmt.Println("Got Bot:", bot)
-
-	//Parsing args
+	//Getting args
 	args := os.Args
 
 	// target people file path -t
@@ -88,230 +82,127 @@ func main() {
 		fmt.Println("Not correct arguments for launch")
 	} else {
 
-		var sourceFilePath string
-		var credsFilePath string
+		var creds Creds
+		//Just getting args from the input launch
+		ParseArgs(args, &creds, bot)
 
-		for index, arg := range args{
-			if arg == "-t"{
-				sourceFilePath = args[index+1]
-			}
-			if arg == "-c"{
-				credsFilePath = args[index+1]
-			}
-		}
-
-		if len(credsFilePath) == 0{
-			fmt.Errorf("please use correct arguments -t for target people, -c for credentrials")
-			os.Exit(400)
-		}
-
-		if len(sourceFilePath) != 0{
-			// read the whole file at once
-			if len(sourceFilePath)>0{
-				sourceF, err := ioutil.ReadFile(sourceFilePath)
-				if err != nil {
-					panic(err)
-				}
-				json.Unmarshal(sourceF, &targetFollowers)
-			}
-
-			fmt.Println("Got JARR from file len:",len(targetFollowers))
-
-			if len(targetFollowers) > 0{
-				//Fill new users to database with exist check
-				for _, followerRaw := range targetFollowers{
-					var user store.User
-					bot.userStore.GetUserById(followerRaw.UserId, &user)
-					if user.UserID != followerRaw.UserId{
-						user = store.User{Name:followerRaw.Name,UserID:followerRaw.UserId,Description:followerRaw.Description,Weight:followerRaw.Weight,Status:0,LastActionTime:time.Now().Unix() }
-						bot.userStore.Insert(user)
-					}
-				}
-			}
-		}
-		// read the whole file at once
-		b, err := ioutil.ReadFile(credsFilePath)
-		if err != nil {
-			panic(err)
-		}
-		json.Unmarshal(b, &creds)
-
-		fmt.Println("Got JARR from file:",creds)
-
-		fmt.Println("SOurce FIle Path:", sourceFilePath)
-		fmt.Println("Creds FIle Path:", credsFilePath)
-
-
-
-		//Here we are ready to start normal work of the people
-		twClient = initTwitterClient(&creds)
+		//Just twitter client initialization
+		twClient := initTwitterClient(&creds)
 
 		fmt.Println("Client Initialized Ok:", twClient)
-		//fmt.Printf("TWITTER CLIENT pointer:", twClient)
 
-		//
-		//config := oauth1.NewConfig(creds.ConsumerKey, creds.ConsumerSecret)
-		//token := oauth1.NewToken(creds.Token, creds.TokenSecret)
-		//// http.Client will automatically authorize Requests
-		//httpClient := config.Client(oauth1.NoContext, token)
-		//
-		//// twitter client
-		//client := twitter.NewClient(httpClient)
-		//
-		//fmt.Printf("TWITTER CLIENT pointer:", client)
-		//time.Sleep(10 * time.Second)
-		//fmt.Println("Client Initialized Ok:", client)
-
-		//all, err := bot.userStore.GetAllUsers()
-		//fmt.Printf("ALL: %d , err:%s \n", len(all), err)
-		//usersToFollow,err := bot.userStore.GetUsersToFollow()
-		//fmt.Printf("USEROT F: %d , err:%s \n", len(usersToFollow), err)
-
-
-		//StartFollowUnFollow(&twClient)
-		//ALL IN ONE SHIT CODE
-
-
-		var running bool
-		running = true
-
-		var allUsersCount, toFollowUsersCount, toUnFollowUsersCount, usersDoneCount int64
-
-
-
-		usersToFollow, err := bot.userStore.GetUsersToFollow()
-		if err != nil{
-			fmt.Errorf("getToUnFollow err:", err)
-		}
-		usersToUnFollow,err := bot.userStore.GetUsersToUnFollow()
-
-		allUsers, _ := bot.userStore.GetAllUsers()
-
-		allUsersCount = int64(len(allUsers))
-		toFollowUsersCount = int64(len(usersToFollow))
-		toUnFollowUsersCount = int64(len(usersToUnFollow))
-		usersDoneCount = allUsersCount - toFollowUsersCount - toUnFollowUsersCount
-
-
-		fmt.Printf("Wokr need to be done for %d persents\n to follow %d \n to unfollow %d \n done: %d \n", (usersDoneCount/allUsersCount)*100, toFollowUsersCount, toUnFollowUsersCount, usersDoneCount)
-
-		var followers int
-
-		for running {
-
-			//toUnfollow, _ := bot.userStore.GetUsersToUnFollow()
-			//toFollow, _ := bot.userStore.GetUsersToFollow()
-			pending, _ := bot.userStore.GetUsersByStatus(1)
-
-			action := ChoseAction(len(usersToFollow), len(usersToUnFollow), followers, len(pending) )
-			fmt.Printf("\n Status\n to follow: %d \n to unfollow: %d \n action: %d", len(usersToFollow), len(usersToUnFollow), action)
-			if action == 1 {
-
-				toFollowUser := usersToFollow[0]
-
-				if Follow(&toFollowUser, twClient){
-					toFollowUser.Status = 1
-					toFollowUser.LastActionTime = time.Now().Unix()
-					bot.userStore.Update(toFollowUser)
-				}
-				usersToFollow = usersToFollow[1:]
-
-				time.Sleep(waitTime * time.Second)
-
-			} else if action == 2{
-				//toUnFollowArr,_ := bot.userStore.GetUsersToUnFollow()
-				toUnfollow := usersToUnFollow[0]
-
-				if UnFollow(&toUnfollow, twClient){
-					toUnfollow.Status = 2
-					toUnfollow.LastActionTime = time.Now().Unix()
-					bot.userStore.Update(toUnfollow)
-				}
-				time.Sleep(waitTime * time.Second)
-				usersToUnFollow = usersToUnFollow[1:]
-			} else if action == 3 {
-				//TODO implement wait time
-			} else{
-				running = false
-			}
-		}
-
-		fmt.Printf("Work is done, enjoy it ;)")
-
+		//Here we are ready to start normal work of the people
+		StartFollowUnFollow(twClient, bot)
 
 	}
 
 }
 
-func StartFollowUnFollow(client *twitter.Client)  {
 
-	//var stop bool
-	//stop = false
-	//
-	//var allUsersCount, toFollowUsersCount, toUnFollowUsersCount, usersDoneCount int64
-	//
-	//
-	//
-	//usersToFollow,_ := bot.userStore.GetUsersToUnFollow()
-	//usersToUnFollow,_ := bot.userStore.GetUsersToUnFollow()
-	//
-	//allUsers, _ := bot.userStore.GetAllUsers()
-	//
-	//allUsersCount = int64(len(allUsers))
-	//toFollowUsersCount = int64(len(usersToFollow))
-	//toUnFollowUsersCount = int64(len(usersToUnFollow))
-	//usersDoneCount = allUsersCount - toFollowUsersCount - toUnFollowUsersCount
-	//
-	//
-	//fmt.Printf("Wokr done for %d persents\n to follow %d \n to unfollow %d \n done: %d \n", (usersDoneCount/allUsersCount)*100, toFollowUsersCount, toUnFollowUsersCount, usersDoneCount)
-	//
-	//
-	//for stop != true {
-	//	action := ChoseAction()
-	//
-	//	if action == 1 {
-	//
-	//		toFollowArr,_ := bot.userStore.GetUsersToFollow()
-	//		toFollow := toFollowArr[0]
-	//		fmt.Print("To Follow: %d \n", len(toFollowArr))
-	//		if Follow(&toFollow, client){
-	//			toFollow.Status = 1
-	//			toFollow.LastActionTime = time.Now().Unix()
-	//			bot.userStore.Update(toFollow)
-	//		}
-	//		time.Sleep(waitTime * time.Second)
-	//
-	//	} else if action == 2{
-	//		toUnFollowArr,_ := bot.userStore.GetUsersToUnFollow()
-	//		toUnfollow := toUnFollowArr[0]
-	//		fmt.Print("To UnFollow: %d \n", len(toUnFollowArr))
-	//		if UnFollow(&toUnfollow, client){
-	//			toUnfollow.Status = 2
-	//			toUnfollow.LastActionTime = time.Now().Unix()
-	//			bot.userStore.Update(toUnfollow)
-	//		}
-	//		time.Sleep(waitTime * time.Second)
-	//
-	//	} else{
-	//		stop = true
-	//	}
-	//}
+func StartFollowUnFollow(twClient *twitter.Client, bot *Bot)  {
+
+	var running bool
+	running = true
+
+	var allUsersCount, toFollowUsersCount, toUnFollowUsersCount int
+
+	var usersToUnFollow []store.User
+
+	usersToFollow, err := bot.userStore.GetUsersToFollow()
+	if err != nil{
+		fmt.Errorf("getToUnFollow err:", err)
+	}
+	usersToUnFollow,_ = bot.userStore.GetUsersToUnFollow()
+
+	allUsers, _ := bot.userStore.GetAllUsers()
+
+	allUsersCount = len(allUsers)
+	toFollowUsersCount = len(usersToFollow)
+	toUnFollowUsersCount = len(usersToUnFollow)
+	usersDone ,_ := bot.userStore.GetUsersByStatus(2)
+
+
+	fmt.Printf("Users stats:\n all users %d \n to follow %d \n to unfollow %d \n done: %d \n", allUsersCount, toFollowUsersCount, toUnFollowUsersCount, len(usersDone))
+
+	//var followers int
+	//time.Sleep(10 * time.Second)
+	for running {
+
+		//if it will be bad use it
+		//pending, _ := bot.userStore.GetUsersByStatus(1)
+
+		//This need to update cause it depends on the time
+		usersToUnFollow,_ = bot.userStore.GetUsersToUnFollow()
+
+		//action := ChoseAction(len(usersToFollow), len(usersToUnFollow), followers, len(pending) )
+		action := ChoseAction(len(usersToFollow), len(usersToUnFollow))
+		fmt.Printf("\n Status\n to follow: %d \n to unfollow: %d \n action: %d", len(usersToFollow), len(usersToUnFollow), action)
+		if action == 1 {
+
+			toFollowUser := usersToFollow[0]
+
+			if Follow(&toFollowUser, twClient){
+				toFollowUser.Status = 1
+				toFollowUser.LastActionTime = time.Now().Unix()
+				bot.userStore.Update(toFollowUser)
+			}
+			usersToFollow = usersToFollow[1:]
+
+			time.Sleep(waitTime * time.Second)
+
+		} else if action == 2{
+			//toUnFollowArr,_ := bot.userStore.GetUsersToUnFollow()
+			toUnfollow := usersToUnFollow[0]
+
+			if UnFollow(&toUnfollow, twClient){
+				toUnfollow.Status = 2
+				toUnfollow.LastActionTime = time.Now().Unix()
+				bot.userStore.Update(toUnfollow)
+			}
+			time.Sleep(waitTime * time.Second)
+			usersToUnFollow = usersToUnFollow[1:]
+		} else if action == 3 {
+			//TODO update user's followers count
+			//TODO implement wait time for 5 minutes
+		} else{
+			running = false
+		}
+	}
+
+	fmt.Printf("Work is done, enjoy it ;)")
 
 	fmt.Printf("Work is done, enjoy it ;)")
 
 }
 
-func ChoseAction(toFCount int, toUnFCount int, followersCount int, pendingCount int) int {
+//func ChoseAction(toFCount int, toUnFCount int, followersCount int, pendingCount int) int {
+func ChoseAction(toFCount int, toUnFCount int) int {
 	//This method should return 1 to implement follow, 2 to implement unfollow, 3 to wait human factor,  0 - work is done
 
-	if followersCount!= 0 && pendingCount/followersCount >= 2{
-		if toUnFCount == 0{
-			return 3
-		} else {
-			return 2
-		}
-	}
+	//if followersCount!= 0 && pendingCount/followersCount >= 2{
+	//	if toUnFCount == 0{
+	//		return 3
+	//	} else {
+	//		return 2
+	//	}
+	//}
 
+	//old logic
+	//if toUnFCount == 0{
+	//	if toFCount > 0{
+	//		return 1
+	//	} else {
+	//		return 0
+	//	}
+	//} else if toUnFCount <= frame{
+	//	if toFCount > 0 {
+	//		return 1
+	//	} else {
+	//		return 2
+	//	}
+	//}  else if toUnFCount > frame {
+	//	return 2
+	//}
 
 	if toUnFCount == 0{
 		if toFCount > 0{
@@ -319,15 +210,10 @@ func ChoseAction(toFCount int, toUnFCount int, followersCount int, pendingCount 
 		} else {
 			return 0
 		}
-	} else if toUnFCount <= frame{
-		if toFCount > 0 {
-			return 1
-		} else {
-			return 2
-		}
-	}  else if toUnFCount > frame {
+	} else {
 		return 2
 	}
+
 	return 0
 }
 
@@ -407,6 +293,7 @@ func UnFollow(user *store.User, client *twitter.Client) bool {
 		fmt.Printf("\nUnFollowed: %s id: %d \n", user.Name, user.UserID)
 	} else{
 		fmt.Printf("\nUnFollowed ERR: %s id: %d \n Error: %s", user.Name, user.UserID, err)
+		return false
 	}
 	return true
 }
@@ -423,12 +310,69 @@ func Follow(user *store.User, client *twitter.Client) bool {
 		fmt.Printf("\nFollowed: %s id: %d \n", user.Name, user.UserID)
 	} else{
 		fmt.Printf("\nFollowed ERR: %s id: %d \n Error: %s", user.Name, user.UserID, err)
+		return false
 	}
 
 
 	return true
 }
 
+func ParseArgs(args []string, creds *Creds, bot *Bot){
+	var sourceFilePath string
+	var credsFilePath string
+
+	targetFollowers := make([]ToFollowUser,0)
+
+	for index, arg := range args{
+		if arg == "-t"{
+			sourceFilePath = args[index+1]
+		}
+		if arg == "-c"{
+			credsFilePath = args[index+1]
+		}
+	}
+
+	if len(credsFilePath) == 0{
+		fmt.Errorf("please use correct arguments -t for target people, -c for credentrials")
+		os.Exit(400)
+	}
+
+	if len(sourceFilePath) != 0{
+		// read the whole file at once
+		if len(sourceFilePath)>0{
+			sourceF, err := ioutil.ReadFile(sourceFilePath)
+			if err != nil {
+				panic(err)
+			}
+			json.Unmarshal(sourceF, &targetFollowers)
+		}
+
+		fmt.Println("Got JARR from file len:",len(targetFollowers))
+
+		if len(targetFollowers) > 0{
+			//Fill new users to database with exist check
+			for _, followerRaw := range targetFollowers{
+				var user store.User
+				bot.userStore.GetUserById(followerRaw.UserId, &user)
+				if user.UserID != followerRaw.UserId{
+					user = store.User{Name:followerRaw.Name,UserID:followerRaw.UserId,Description:followerRaw.Description,Weight:followerRaw.Weight,Status:0,LastActionTime:time.Now().Unix() }
+					bot.userStore.Insert(user)
+				}
+			}
+		}
+	}
+	// read the whole file at once
+	b, err := ioutil.ReadFile(credsFilePath)
+	if err != nil {
+		panic(err)
+	}
+	json.Unmarshal(b, &creds)
+
+	fmt.Println("Got JARR from file:",creds)
+
+	fmt.Println("SOurce FIle Path:", sourceFilePath)
+	fmt.Println("Creds FIle Path:", credsFilePath)
+}
 
 //func Init(conf *Configuration) (*Multy, error) {
 func Init(conf *Configuration) (*Bot, error) {
