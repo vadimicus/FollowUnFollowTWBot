3d0c35bdd00b33d07437643baf46c4283714f533
vadimicus <vadim.makovsky@gmail.com>
2018-12-21 13:04:33 +0300

    added status implementation

--- client/rest.go
+++ client/rest.go
@@ -3,7 +3,6 @@ package client
 import (
 	"github.com/vadimicus/FollowUnFollowTWBot/store"
 	"github.com/gin-gonic/gin"
-	"time"
 	"net/http"
 )
 
@@ -22,36 +21,47 @@ func SetRestHandlers(
 		userStore:userDB,
 	}
 
-	r.GET("/test", restClient.Test())
+	r.GET("/ping", restClient.Ping())
 
 	return restClient, nil
 }
 
-func (restClient *RestClient) Test() gin.HandlerFunc {
+func (restClient *RestClient) Ping() gin.HandlerFunc {
 	return func(c *gin.Context) {
-		resp := map[string]interface{}{
-			"stockexchanges": map[string][]string{
-				"poloniex": []string{"usd_btc", "eth_btc", "eth_usd", "btc_usd"},
-				"gdax":     []string{"eur_btc", "usd_btc", "eth_btc", "eth_usd", "eth_eur", "btc_usd"},
-			},
-			"servertime": time.Now().UTC().Unix(),
-			"api":        "1.2",
-			//"version":    restClient.MultyVersion,
-			//"donate":     restClient.donationAddresses,
-			"multisigfactory": map[string]string{
-				"ethtestnet": "0x04f68589f53cfdf408025cd7cea8a40dbf488e49",
-				"ethmainnet": "0xc2cbdd9b58502cff1db5f9cce48ac17a9a550185",
-			},
-			//"erc20tokenlist": restClient.ERC20TokenList,
+		all,_ := restClient.userStore.GetAllUsers()
+		to_follow,_ := restClient.userStore.GetUsersToFollow()
+		to_unfollow, _ := restClient.userStore.GetUsersToUnFollow()
+		done, _ := restClient.userStore.GetUsersByStatus(2)
+		followError, _ := restClient.userStore.GetUsersByStatus(66)
+		unFollowError, _ := restClient.userStore.GetUsersByStatus(666)
+
+
+		resp := map[string]interface{}{}
+
+		//resp := map[string]interface{}{
+		//	"stockexchanges": map[string][]string{
+		//		"poloniex": []string{"usd_btc", "eth_btc", "eth_usd", "btc_usd"},
+		//		"gdax":     []string{"eur_btc", "usd_btc", "eth_btc", "eth_usd", "eth_eur", "btc_usd"},
+		//	},
+		//	"servertime": time.Now().UTC().Unix(),
+		//	"api":        "1.2",
+		//	//"version":    restClient.MultyVersion,
+		//	//"donate":     restClient.donationAddresses,
+		//	"multisigfactory": map[string]string{
+		//		"ethtestnet": "0x04f68589f53cfdf408025cd7cea8a40dbf488e49",
+		//		"ethmainnet": "0xc2cbdd9b58502cff1db5f9cce48ac17a9a550185",
+		//	},
+		//	//"erc20tokenlist": restClient.ERC20TokenList,
+		//}
+		resp["users"] = map[string]int{
+			"all": len(all),
+			"to_follow": len(to_follow),
+			"to_unfollow": len(to_unfollow),
+			"done": len(done),
+			"error_to_follow": len(followError),
+			"error_to_unfollow": len(unFollowError),
 		}
-		resp["android"] = map[string]int{
-			"soft": 1,
-			"hard": 1,
-		}
-		resp["ios"] = map[string]int{
-			"soft": 2,
-			"hard": 2,
-		}
+
 
 
 		c.JSON(http.StatusOK, resp)
